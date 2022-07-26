a84faaf7d6519681bab4417b8dd60eca26926e6b
Carla <csachett@student.scf.edu>
2022-07-25 23:37:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backGroundColor = "#FF69B4"; 
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+ document.addEventListener("keydown", function (event) {
+  if (event.key === "arrowLeft") {
+    moveDodgerLeft();
+  }
+  if (event.key === "arrowRight") {
+    moveDodgerRight();
+  }
+ });
+
+
